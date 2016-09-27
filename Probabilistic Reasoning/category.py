111a439f38ff2392136bdcff3c91b3ad2df6f1e6
Magnus Methi <magnus.methi@hotmail.com>
2016-09-27 21:52:46 +0200

    Cleaned up and made some comments

--- Probabilistic Reasoning/category.py
+++ Probabilistic Reasoning/category.py
@@ -2,24 +2,28 @@ import glob
 from copy import deepcopy
 from math import log
 
+
 class Category:
     def __init__(self, path, vocabulary, stop_words, learning_fraction, category_prob):
+        # Keeping path for easy access
         self.path = path
+
+        # Getting files for learning set and test set
         files = glob.glob(path + "\\*")
         files_to_learn = learning_fraction*len(files)
-        self.learning_set = []
+        learning_set = []
         self.test_set = []
         for file in range(len(files)):
             if file < files_to_learn:
-                self.learning_set.append(files[file])
+                learning_set.append(files[file])
             else:
                 self.test_set.append(files[file])
 
-        self.words = []
-        for path in self.learning_set:
+        # Could get copy from outside of class, but easy and quick to repeat
+        words = []
+        for path in learning_set:
             file = open(path, 'r')
             start_parse = False
-
             for line in file:
                 if not start_parse and "Lines:" in line:
                     start_parse = True
@@ -28,26 +32,39 @@ class Category:
                     for w in words_in_line:
                         word = filter(lambda x: x.isalpha(), w).lower()
                         if word not in stop_words and len(word) > 0:
-                            self.words.append(word)
+                            words.append(word)
 
-        relevant_words = set(self.words)
+        # Removing duplicates
+        relevant_words = set(words)
 
+        # Keep in object for use in check_if_document_fits
         self.category_prob = category_prob
+
+        # Deepcopy to avoid getting reference
         self.vocabulary = deepcopy(vocabulary)
-        self.def_prob_for_word = 1.0/(len(self.words) + len(vocabulary))
+
+        # Default probability for words in the vocabulary but not in the category learning set
+        self.def_prob_for_word = 1.0/(len(words) + len(vocabulary))
+
+        # Calculating probability of a word given the category.
+        # Only iterating over words found in the category as all other words will have known values
         for word in relevant_words:
-            word_count = self.words.count(word)
-            p = (word_count + 1.0) / (len(self.words) + len(vocabulary))
+            word_count = words.count(word)
+            p = (word_count + 1.0) / (len(words) + len(vocabulary))
             self.vocabulary[word] = p
 
-    def check_if_docment_fits(self, words_in_document):
+    # Calculating value p. High p means document likely fits the category
+    def check_if_document_fits(self, words_in_document):
         p = self.category_prob
         for word in words_in_document:
             try:
+                # Word is in vocabulary, but not in this category
                 if self.vocabulary[word] == 0:
                     p += log(self.def_prob_for_word)
+                #Word is in vocabulary and this category
                 else:
                     p += log(self.vocabulary[word])
+            # Word is not in the vocabulary
             except KeyError:
                 p += 0
         return p
@@ -57,6 +74,3 @@ class Category:
 
     def get_path(self):
         return self.path
-
-    def get_vocabulary(self):
-        return self.vocabulary
--- Probabilistic Reasoning/main.py
+++ Probabilistic Reasoning/main.py
@@ -43,31 +43,32 @@ def calc_category_prob(category):
             category_files += len(files)
     return category_files/total_files
 
-#Fraction of documents used for learning
-learning_fraction = 0.8#2.0 / 3.0
+# Fraction of documents reserved for learning
+learning_fraction = 2.0 / 3.0
 
-#Listing words to avoid when caluclating probablity
+# Listing words to avoid when caluclating probablity
 stop_words = []
 for line in open("stop_words.txt", 'r'):
     stop_words += line.split()
 
-#Acquiring paths for the documents in each category
+# Acquiring paths for the documents in each category
 category_paths = glob.glob("20_newsgroups\\*")
 
 
-#Get words from learning sets and establishing vocabulary
+# Get words from learning sets and establishing vocabulary
 categories = []
 total_words = []
 category_probs = []
+print "Getting words from learning sets and establishing vocabulary..."
 for i in range(len(category_paths)):
     words = parse_documents(category_paths[i], learning_fraction, stop_words)
     total_words += set(words)
     category_probs.append(calc_category_prob(i))
-    print category_paths[i], "parsed"
 
 vocabulary = dict(zip(set(total_words), [0]*len(set(total_words))))
 
 test_documents = []
+print "Making category objects and calculating word prob given category"
 for i in range(len(category_paths)):
     print "Learning", category_paths[i]
     category = Category(category_paths[i], vocabulary, stop_words, learning_fraction, category_probs[i])
@@ -75,6 +76,7 @@ for i in range(len(category_paths)):
     test_documents += category.get_test_set()
 
 print "Start testing"
+
 correctly_asserted = []
 incorrectly_asserted = []
 for document in test_documents:
@@ -82,16 +84,15 @@ for document in test_documents:
     max_group = ''
     max_p = -float_info.max
     for category in categories:
-        p = category.check_if_docment_fits(words_in_document)
+        p = category.check_if_document_fits(words_in_document)
         if p>max_p:
             max_p = p
             max_group = category.get_path()
-    print max_p
     if max_group in document:
-        correctly_asserted.append(document)
+        correctly_asserted.append([max_group, document])
         print "Correct! Guessed", max_group, "for", document
     else:
-        incorrectly_asserted.append(document)
+        incorrectly_asserted.append([max_group, document])
         print "Wrong! Guessed", max_group, "for", document
 
 print len(correctly_asserted), "right"
@@ -99,3 +100,23 @@ print len(incorrectly_asserted), "wrong"
 
 accuracy = float(len(correctly_asserted))/(len(correctly_asserted)+len(incorrectly_asserted))
 print accuracy*100, "percent accurate"
+
+actual_category_vs_guessed_category = []
+for path in category_paths:
+    category_guessed = []
+    correct = 0.0
+    for guess in correctly_asserted:
+        if path in guess[1]:
+            correct += 1.0
+            category_guessed.append(guess[0])
+    wrong = 0.0
+    for guess in incorrectly_asserted:
+        if path in guess[1]:
+            wrong += 1.0
+            category_guessed.append(guess[0])
+    actual_category_vs_guessed_category.append([path, category_guessed, (correct/(correct+wrong))])
+
+for category in actual_category_vs_guessed_category:
+    # for guess in set(category[1]):
+    #     print category[0], ":", category[1].count(guess), guess
+    print category[0], category[2]*100, "percent accurate"
