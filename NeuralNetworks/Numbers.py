80f8adaff04eb47a8e09bb0767238fc6d00dcb33
Jone Bakkevig <jonekb13@student.uia.no>
2016-10-11 21:33:44 +0200

    added parsing with network initializer

--- NeuralNetworks/Numbers.py
+++ NeuralNetworks/Numbers.py
@@ -40,9 +40,9 @@ def load_data():
     below.
     """
     f = gzip.open('mnist.pkl.gz', 'rb')
-    training_data, validation_data, test_data = cPickle.load(f)
+    training_data, test_data = cPickle.load(f)
     f.close()
-    return (training_data, validation_data, test_data)
+    return (training_data, test_data)
 
 def load_data_wrapper():
     """Return a tuple containing ``(training_data, validation_data,
@@ -69,17 +69,15 @@ def load_data_wrapper():
     training_inputs = [np.reshape(x, (784, 1)) for x in tr_d[0]]
     training_results = [vectorized_result(y) for y in tr_d[1]]
     training_data = zip(training_inputs, training_results)
-    validation_inputs = [np.reshape(x, (784, 1)) for x in va_d[0]]
-    validation_data = zip(validation_inputs, va_d[1])
     test_inputs = [np.reshape(x, (784, 1)) for x in te_d[0]]
     test_data = zip(test_inputs, te_d[1])
-    return (training_data, validation_data, test_data)
+    return (training_data, test_data)
 
 def vectorized_result(j):
-    """Return a 10-dimensional unit vector with a 1.0 in the jth
+    """Return a 4-dimensional unit vector with a 1.0 in the jth
     position and zeroes elsewhere.  This is used to convert a digit
     (0...9) into a corresponding desired output from the neural
     network."""
-    e = np.zeros((10, 1))
+    e = np.zeros((4, 1))
     e[j] = 1.0
     return e
