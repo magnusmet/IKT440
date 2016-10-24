f0440c0b47d808e427063884862bbfd317435ed4
Magnus Methi <Magnus Methi>
2016-10-24 18:52:31 +0200

    Added ability to generate more than one tree with a set depth

--- DecisionTrees/DecisionTree.py
+++ DecisionTrees/DecisionTree.py
@@ -1,14 +1,18 @@
 import csv, math, tools, random
 
+
 data = csv.reader(open("data/tweets.csv", "r"))
 all_data = []
 for d in data:
-    all_data.append([d[1], d[2]])
+    all_data.append([d[1], d[2], d[4], d[10]])
+    if "is_retweet" not in d[3]:
+        if "True" not in d[3] and "False" not in d[3]:
+            print d[3]
+            assert False
 all_data = all_data[1:]
 random.shuffle(all_data)
 training_tweets = all_data[(len(all_data) / 2):]
 trump_words, hillary_words = tools.get_top_words(training_tweets)
-
 
 def contains_most_used_words(tweet):
     global trump_words
@@ -22,6 +26,22 @@ def contains_most_used_words(tweet):
 
     return "Contains no Trump or Hillary words"
 
+def tweet_author(handle):
+    if handle is "":
+        return "OC"
+    elif "trump" in handle.lower() or "donald" in handle.lower():
+        return "Retweeted Trump user"
+    elif "clinton" in handle.lower() or "hillary" in handle.lower():
+        return "Retweeted Hillary user"
+    else:
+        return "Retweet"
+
+def language(lang):
+    if "en" == lang:
+        return "English"
+    else:
+        return "Foreign"
+
 
 def binary_choice(name):
     if "Trump" in name:
@@ -31,11 +51,13 @@ def binary_choice(name):
 
 training_data = []
 for data in all_data[(len(all_data) / 2):]:
-    training_data.append([binary_choice(data[0]), contains_most_used_words(data[1])])
+    training_data.append([binary_choice(data[0]), contains_most_used_words(data[1]),
+                          tweet_author(data[2]), language(data[3])])
 
 verification_data = []
 for data in all_data[:(len(all_data) / 2)]:
-    verification_data.append([binary_choice(data[0]), contains_most_used_words(data[1])])
+    verification_data.append([binary_choice(data[0]), contains_most_used_words(data[1]),
+                              tweet_author(data[2]), language(data[3])])
 
 
 
@@ -70,7 +92,6 @@ def gain(oneclass, attribute):
 
 
 def getHighestGain(oneclass):
-    before = entropy(oneclass)
     classes = [i for i in range(1, len(oneclass[0]))]
     entropies = [gain(oneclass, c) for c in classes]
     return entropies.index(min(entropies)) + 1
@@ -101,31 +122,47 @@ def confidence(oneclass):
 
 actualClassifier = "def classify(data):"
 
-
-def buildTree(oneclass, spaces="    "):
+def buildTree(oneclass, spaces="    ", depth=0):
     global actualClassifier
-    if (isEmpty(oneclass) or isPure(oneclass)):
-        print(spaces, "then ", mostCommon(oneclass))
-        print(spaces, "#confidence", confidence(oneclass))
-        actualClassifier += "\n" + spaces + "return (" + mostCommon(oneclass) + ")"
+    if (isPure(oneclass) or isEmpty(oneclass) or depth is 2):
+        #print(spaces, "then ", mostCommon(oneclass))
+        #print(spaces, "#confidence", confidence(oneclass))
+        actualClassifier += "\n" + spaces + "return " + mostCommon(oneclass)
         return
     highest = getHighestGain(oneclass)
     d = split(oneclass, highest)
     for key, value in d.items():
-        print(spaces, "if ", key)  # ,"(item", highest,")")
+        #print(spaces, "if ", key)  # ,"(item", highest,")")
         actualClassifier += "\n" + spaces + "if(data[" + str(highest) + "]==\"" + str(key) + "\"):"
-        buildTree(value, spaces + "   ")
+        buildTree(value, spaces + "   ", depth+1)
 
+n_chunks = 10
+chunk = len(training_data)/n_chunks
+rest_of_set = training_data
+answers = []
+for i in range(n_chunks):
+    actualClassifier = "def classify(data):"
+    buildTree(rest_of_set[:chunk])
+    rest_of_set = rest_of_set[chunk:]
+    print(actualClassifier)
+    exec actualClassifier
+    for j in range(len(verification_data)):
+        answer = classify(verification_data[j])
+        try:
+            answers[j].append(answer)
+        except:
+            answers.append([answer])
 
-buildTree(training_data)
-print(actualClassifier)
-exec (actualClassifier)
 correct, wrong = 0.0, 0.0
-for data in verification_data:
-    if (int(data[0]) == int(classify(data))):
+for i in range(len(verification_data)):
+    final_answer = "0"
+    if answers[i].count(1) > len(answers[i])/2:
+        final_answer = "1"
+    if verification_data[i][0] is final_answer:
         correct += 1
     else:
         wrong += 1
+
 print ("Correct classifications", correct)
 print ("Wrong classifications", wrong)
 print ("Accuracy", correct / (correct + wrong))
