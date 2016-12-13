ff1f7d10c35b47d9632127d2e0fed106af493db3
Jone Bakkevig <jonekb13@student.uia.no>
2016-12-13 20:28:01 +0100

    added waterlevel class and production class with intitial calculation for income

--- Project_Agder/Tsetlin.py
+++ Project_Agder/Tsetlin.py
@@ -1,21 +1,106 @@
 import random
 
 
+
+class Production:
+    def __init__(self):
+        self.price = [15,15,8,14,9,10,7,14,13,9,12,11,10,10,8,5,5,14,9,5,8,9,10,7]
+        #self.price = [7,7,7,7,7,7,7,7,7,7,7,7,7,8,8,8,8,8,8,8,8,8,8,8]
+        self.max_profit = 15*24 #maxprice*24h
+        self.efficiency = []
+
+    def production(self, t, water_level):
+        if water_level == 0:
+            return 0
+        efficiency = water_level/50.0 #max_wl/wl
+
+        profit = self.price[t]*efficiency
+
+        return profit
+
+
+
+
+
+class WaterLevel:
+    def __init__(self):
+
+        self.water_level = 0.0
+        self.outflow = 4.0
+        self.inflow = []
+
+
+        for i in range(0,24):
+            self.inflow.append(random.randrange(10.0,50.0)/10.0)
+
+
+    def openOrClose(self, t, open):
+        if open:
+            if self.inflow[t] > self.outflow:
+                difference = self.inflow[t] - self.outflow
+                if self.water_level >= 50.0:
+                    self.water_level = 50.0
+
+                else:
+                    self.water_level += difference
+
+            if self.outflow > self.inflow[t]:
+                difference = self.outflow - self.inflow[t]
+                if self.water_level == 0:
+                    return -1
+                else:
+                    self.water_level -= difference
+
+        else:
+            self.water_level += self.inflow[t]
+            if self.water_level >= 50.0:
+                self.water_level = 50.0
+
+        return self.water_level
+
+    def waterLevel(self, production):
+
+        if self.inflow>self.outflow:
+            difference = self.inflow-self.outflow
+            if self.water_level >= 50.0:
+                self.water_level = 50.0
+                return self.water_level
+            else:
+                self.water_level+=difference
+                return self.water_level
+        if self.outflow>self.inflow:
+            difference = self.outflow-self.inflow
+            if self.water_level==0:
+                return -1
+            else:
+                self.water_level -= difference
+                return self.water_level
+
+        if production == True:
+            self.water_level -= self.outflow
+            return self.water_level
+        else:
+            return self.water_level
+
+    def getWaterlevel(self):
+        return self.water_level
+
+    def setWaterlevel(self):
+        self.water_level = 0.0
+
 class Judge:
     def __init__(self):
-        self.reward_prob = [0.0, 0.2, 0.4, 0.6, 0.4, 0.2]
+        self.reward_prob = 0.0
 
-    def reward(self, voters):
-        number_of_yes_votes = voters.count(1)
+    def reward_probability(self, profit):
+        self.reward_prob = profit/(100) #profit/maxprofit
 
-        rewards = []
-        for voter in voters:
-            if random.random() <= self.reward_prob[number_of_yes_votes]:
-                rewards.append(True)
-            else:
-                rewards.append(False)
-        return rewards
 
+    def reward(self):
+        if random.random() <= self.reward_prob:
+            return True
+        else:
+            return False
 
 class Tsetlin:
     def __init__(self, n):
--- Project_Agder/main.py
+++ Project_Agder/main.py
@@ -1,9 +1,50 @@
-from Tsetlin import Judge, Tsetlin
+from Tsetlin import Production, Tsetlin, Judge, WaterLevel
+
+
+
+
+
 
 
 states = 10
-
 judge = Judge()
-voters = []
-for voter in range(5):
-    voters.append(Tsetlin(states+1))
+prod = Production()
+automatas = []
+wl = WaterLevel()
+
+for automata in range(24):
+    automatas.append(Tsetlin(states))
+
+totalIncome = 0.0
+
+for k in range(100000):
+    decisions = []
+    income = 0.0
+
+    wl.setWaterlevel()
+
+    for automata in automatas:
+        decisions.append(automata.makeDecision())
+
+    for t in range(len(decisions)):
+        if decisions[t] == 1:
+            income += prod.production(t,wl.getWaterlevel())
+            wl.openOrClose(t,True)
+
+        else:
+            wl.openOrClose(t,False)
+
+    judge.reward_probability(income)
+    for i in automatas:
+        i.reward_or_penalty(judge.reward())
+
+
+
+    totalIncome+=income
+print 'max profit : 150'
+print 'profit:', income
+print 'avg income', totalIncome/100000
+#judge = Judge()
+#voters = []
+#for voter in range(5):
+    #voters.append(Tsetlin(states+1))
